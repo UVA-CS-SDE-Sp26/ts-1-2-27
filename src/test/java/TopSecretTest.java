f1ff2f436fb94cc2d5b881bd83a590a4979a0ab0
Owen Kearney <meepowen123@gmail.com>
2026-02-09 16:34:25 -0500

    Created TopSecretTest.java
    added tests to filehandler.txt, added a basic flow and my methods to TopSecret, created top secret test file

---
+++ src/test/java/TopSecretTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TopSecretTest {
+    //just made a general TopSecret test file
+    //unless we need other classes we can probably just add the rest of the tests to this one
+
+    @Test
+    void files() {
+    }
+
+    @Test
+    void locateFile() {
+    }
+}
