a40498a0d4c118b9eb90539f2cef80428cecbe66
Owen Kearney <meepowen123@gmail.com>
2026-02-10 14:14:47 -0500

    Added Filehandler.java
    Moved the filehandler stuff from TopSecret to its own class and test file.

---
+++ src/main/java/Filehandler.java
@@ -0,0 +1,13 @@
+public class Filehandler {
+
+    public Filehandler() {}
+
+
+    public static String[] files(){
+        return null;
+    }
+
+    public static String getFile(String fileNumber) {
+        return null;
+    }
+}
---
+++ src/test/java/FilehandlerTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilehandlerTest {
+
+    @Test
+    void files() {
+    }
+
+    @Test
+    void getFile() {
+    }
+}
